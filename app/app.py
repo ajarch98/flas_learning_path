a92c76fb2abec3e760ddff427c932e8e6d59daf0
Advait Joshi <advaitjoshi1998@gmail.com>
2019-07-31 20:49:38 +0300

    Template with for and if

--- app/app.py
+++ app/app.py
@@ -6,4 +6,14 @@ app = Flask(__name__)
 @app.route('/index')
 def index():
     user = {'username':'Advait'}
-    return render_template("index.html", title = "Hello", user = user)
+    posts = [
+        {
+            'author': {'username':'John'},
+            'body': 'Beautiful day in Portland!!'
+        },
+        {
+            'author':{'username':'Susan'},
+            'body':'The Avengers movie was amazing!!'
+        }
+    ]
+    return render_template("index.html", title = "Hello to Microblog", user = user, posts = posts)
