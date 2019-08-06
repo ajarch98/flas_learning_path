4943d966b9f7143d9f725150eec14ff4a617ef9b
Advait Joshi <advaitjoshi1998@gmail.com>
2019-08-06 10:26:28 +0300

    Added logging and fixed username validation error

--- app/__init__.py
+++ app/__init__.py
@@ -4,6 +4,10 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_login import LoginManager
 
+import logging
+from logging.handlers import RotatingFileHandler
+import os
+
 App = Flask(__name__)
 App.config.from_object(Config)
 db = SQLAlchemy(App)
@@ -11,4 +15,15 @@ migrate = Migrate(App, db)
 login = LoginManager(App)
 login.login_view = 'login'
 
-from app import routes, models
+from app import routes, models, errors
+
+if not App.debug:
+    if not os.path.exists('logs'):
+        os.mkdir('logs')
+    file_handler = RotatingFileHandler('logs/microblog.log', maxBytes = 10240, backupCount = 10)
+    file_handler.setFormatter(logging.Formatter('%(asctime)s %(levelname)s : %(message)s [in %(pathname)s: %(lineno)d]'))
+    file_handler.setLevel(logging.INFO)
+    App.logger.addHandler(file_handler)
+
+    App.logger.setLevel(logging.INFO)
+    App.logger.info('Microblog startup')
--- app/routes.py
+++ app/routes.py
@@ -6,11 +6,15 @@ from app.models import User
 from werkzeug.urls import url_parse
 from datetime import datetime
 
+
+
 @App.before_request
 def before_request():
     if current_user.is_authenticated:
         current_user.last_seen = datetime.utcnow()
         db.session.commit()
+
+
 
 @App.route('/')
 @App.route('/index')
@@ -29,6 +33,8 @@ def index():
     ]
     return render_template("index.html", title = "Hello to Microblog", posts = posts)
 
+
+
 @App.route('/login', methods = ['GET', 'POST'])
 def login():
     if current_user.is_authenticated:
@@ -40,16 +46,19 @@ def login():
             flash('Invalid username or password')
             return redirect(url_for('login'))
         login_user(user, remember=form.remember_me.data)
+        App.logger.info('{} logged in successfully'.format(user.username))
         next_page = request.args.get('next')
         if not next_page or url_parse(next_page).netloc != '':
             next_page = url_for('index')
         return redirect(next_page)
     return render_template("login_form.html", title = "Sign In", form = form)
 
+
+
 @App.route('/edit_profile', methods = ['GET', 'POST'])
 @login_required
 def edit_profile():
-    form = EditProfileForm()
+    form = EditProfileForm(current_user.username)
     if form.validate_on_submit():
         current_user.username = form.username.data
         current_user.about_me = form.about_me.data
@@ -61,10 +70,14 @@ def edit_profile():
         form.about_me.data = current_user.about_me
     return render_template('edit_profile_form.html', title = "Edit Profile", form = form)
 
+
+
 @App.route('/logout')
 def logout():
     logout_user()
     return redirect(url_for('index'))
+
+
 
 @App.route('/register', methods = ['GET', 'POST'])
 def register():
@@ -79,6 +92,8 @@ def register():
         flash('Congratulations, you are now a registered user')
         return redirect(url_for('login'))
     return render_template('register.html', title = 'Register', form=form)
+
+
 
 @App.route('/user/<username>')
 @login_required
